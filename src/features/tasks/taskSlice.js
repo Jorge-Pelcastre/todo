cb85221e2498c1fc72c57c5aff7421b29ab26ab3
jorge-pelcastre <jorgepelcastre@outlook.com>
2022-11-22 15:16:07 -0600

    taskSlice

---
+++ src/features/tasks/taskSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [
+    {
+        id: '1',
+        title: 'task 1',
+        description: 'taks 1 description',
+        complete: false
+    },
+    {
+        id: '2',
+        title: 'task 2',
+        description: 'taks 1 description',
+        complete: false
+    }
+]
+
+export const taskSlice = createSlice({
+    name: 'tasks',
+    initialState,
+    reducers: {
+        
+    }
+})
+
+export default taskSlice.reducer;
