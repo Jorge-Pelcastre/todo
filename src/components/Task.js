24e37b38d37e9d71c63074d2592d37daed71e314
jorge-pelcastre <jorgepelcastre@outlook.com>
2022-11-23 13:44:03 -0600

    style: tasks list style
    list  and items tasks new style

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,6 +1,9 @@
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { deleteTask } from "../features/tasks/taskSlice";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit, faTrash, faSquareCheck } from '@fortawesome/free-solid-svg-icons';
+import { faSquare } from '@fortawesome/free-regular-svg-icons';
 
 function Task({ id, title, description, complete }) {
     const dispatch = useDispatch();
@@ -10,17 +13,32 @@ function Task({ id, title, description, complete }) {
     }
 
     return (
-        <div>
-            <h3>
-                <input type='checkbox' value={complete} />
-                {title}
-                <button onClick={e => handleDelete(id)}>x</button>
-                <Link to={`/${id}/edit`}>Edit</Link>   
-            </h3>
-            <p>
-                {description}
-            </p>
-        </div>
+        <li className="task">
+            <div className="task-check">
+                <FontAwesomeIcon icon={complete ? faSquareCheck : faSquare}/>
+            </div>
+            <div className="task-content">
+                <div className="task-title">
+                    <strong>
+                        {title}   
+                    </strong>
+                    <span>
+                        <button onClick={e => handleDelete(id)} className="cancel-button">
+                            <FontAwesomeIcon icon={faTrash}/>
+                        </button>
+                        <Link to={`/${id}/edit`}>
+                            <FontAwesomeIcon icon={faEdit}/>
+                        </Link>
+                    </span>
+                </div>
+                <p className="task-description">
+                    {description}
+                </p>
+                <div>
+                    
+                </div>
+            </div>
+        </li>
     );
 }
 
--- src/components/TasksList.js
+++ src/components/TasksList.js
@@ -1,16 +1,22 @@
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlus } from '@fortawesome/free-solid-svg-icons'
 import Task from './Task';
 
 function TasksList() {
     const tasks = useSelector(state => state.tasks);
 
     return (
-        <div>
+        <ul className='tasks-list'>
             {tasks.map(({id, title, description, complete}) => 
             <Task key={id} id={id} title={title} description={description} complete={complete}/>)}
-            <Link to="/create">Create</Link>
-        </div>
+            <li className="add-task">
+                <Link to="/create" className='add-task-link'>
+                    <FontAwesomeIcon icon={faCirclePlus} size="2xl" />
+                </Link>
+            </li>
+        </ul>
     );
 }
 
