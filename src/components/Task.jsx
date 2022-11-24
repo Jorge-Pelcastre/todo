4fe04210e3f4957fb5dd4bd6f12b1ce1c3a4f158
jorge-pelcastre <jorgepelcastre@outlook.com>
2022-11-24 13:08:27 -0600

    feat: task delete confirmation

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,42 +1,63 @@
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEdit, faTrash, faSquareCheck } from '@fortawesome/free-solid-svg-icons';
-import { faSquare } from '@fortawesome/free-regular-svg-icons';
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEdit,
+  faTrash,
+  faSquareCheck
+} from "@fortawesome/free-solid-svg-icons";
+import { faSquare } from "@fortawesome/free-regular-svg-icons";
 import { deleteTask } from "../features/tasks/taskSlice";
 
+const reactSwal = withReactContent(Swal);
+
 function Task({ id, title, description, complete }) {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const handleDelete = (idTask) => {
+  const handleDelete = (idTask) => {
+    reactSwal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "purple",
+      confirmButtonText: "Delete"
+    }).then((result) => {
+      if (result.isConfirmed) {
         dispatch(deleteTask(idTask));
-    }
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
+      }
+    });
+  };
 
-    return (
-        <li className="task">
-            <div className="task-check">
-                <FontAwesomeIcon icon={complete ? faSquareCheck : faSquare}/>
-            </div>
-            <div className="task-content">
-                <div className="task-title">
-                    <strong>
-                        {title}   
-                    </strong>
-                    <span>
-                        <button type="button" onClick={() => handleDelete(id)} className="cancel-button">
-                            <FontAwesomeIcon icon={faTrash}/>
-                        </button>
-                        <Link to={`/${id}/edit`}>
-                            <FontAwesomeIcon icon={faEdit}/>
-                        </Link>
-                    </span>
-                </div>
-                <p className="task-description">
-                    {description}
-                </p>
-            </div>
-        </li>
-    );
+  return (
+    <li className="task">
+      <div className="task-check">
+        <FontAwesomeIcon icon={complete ? faSquareCheck : faSquare} />
+      </div>
+      <div className="task-content">
+        <div className="task-title">
+          <strong>{title}</strong>
+          <span>
+            <button
+              type="button"
+              onClick={() => handleDelete(id)}
+              className="cancel-button"
+            >
+              <FontAwesomeIcon icon={faTrash} style={{color: "purple"}}/>
+            </button>
+            <Link to={`/${id}/edit`}>
+              <FontAwesomeIcon icon={faEdit} />
+            </Link>
+          </span>
+        </div>
+        <p className="task-description">{description}</p>
+      </div>
+    </li>
+  );
 }
 
 export default Task;
